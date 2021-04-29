4b17ae8279930c74e42cdbec812dc8f5ab1b6f8f
Clint Hansen <clint.hansen2009@gmail.com>
2021-04-28 23:03:40 -0600

    sequelize connection

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+let sequelize;
+
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
+
