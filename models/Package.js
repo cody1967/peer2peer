b5d12141c8779e9eb0bdb734ebfd02f489656c72
Clint Hansen <clint.hansen2009@gmail.com>
2021-04-29 19:30:18 -0600

    package model

---
+++ models/Package.js
@@ -0,0 +1,82 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Package extends Model {}
+
+Package.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        pick_up_state: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pick_up_city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pick_up_street: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pick_up_zip: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        drop_off_state: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        drop_off_city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        drop_off_street: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        drop_off_zip: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        deliver_by: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+            validate: {
+                isDate: true,
+                isAfter: DataTypes.NOW
+            }
+        },
+        item: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        driver_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'driver',
+                key: 'id'
+            }
+        },
+        client_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'client',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        timestamps: true,
+        underscored: true,
+        modelName: 'package'
+    }
+);
+
+model.exports = Package;
