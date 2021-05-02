ddddb35db5632b1830f5e6c2c37e6e0737e80a38
Clint Hansen <clint.hansen2009@gmail.com>
2021-05-02 00:10:06 -0600

    basic client/driver routes

--- controllers/api/driver-routes.js
+++ controllers/api/driver-routes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const { Client, Driver, Package } = require('../../models');
 
+// all drivers
 router.get('/', (req, res) => {
-    Driver.findAll()
+    Driver.findAll({
+        attributes: { exclude: ['password'] }
+    })
     .then(dbDriverData => res.json(dbDriverData))
     .catch(err => {
         console.log(err);
@@ -10,14 +13,82 @@ router.get('/', (req, res) => {
     });
 });
 
+// get single driver by id
+router.get('/:id', (req, res) => {
+    Driver.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Client,
+                attributes: ['first_name', 'last_name', 'email'],
+                through: Package,
+                as: 'assigned_client'
+            }
+        ]
+    })
+    .then(dbDriverData => {
+        if (!dbDriverData) {
+            res.status(404).json({message: 'No driver found with this id'});
+            return;
+        }
+        res.json(dbDriverData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+}); 
+
 router.post('/', (req, res) => {
     Driver.create(req.body)
-    .then(dbClientData => res.json(dbClientData))
+    .then(dbDriverData => res.json(dbDriverData))
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
 
+// update driver info
+router.put('/:id', (req, res) => {
+    Driver.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbDriverData => {
+        if (!dbDriverData[0]) {
+            res.status(404).json({ message: 'No driver found with this id' });
+            return;
+        }
+        res.json(dbDriverData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    }); 
+});
+
+// delete a driver
+router.delete('/:id', (req, res) => {
+    Driver.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbDriverData => {
+        if (!dbDriverData) {
+            res.status(404).json({ message: 'No driver found with this id' });
+            return;
+        }
+        res.json(dbDriverData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- controllers/api/package-routes.js
+++ controllers/api/package-routes.js
@@ -10,6 +10,27 @@ router.get('/', (req, res) => {
     });
 });
 
+// get single package by id
+router.get('/:id', (req, res) => {
+    Client.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbClientData => {
+        if (!dbClientData) {
+            res.status(404).json({message: 'No client found with this id'});
+            return;
+        }
+        res.json(dbClientData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});  
+
+
 router.post('/', (req, res) => {
     Package.create(req.body)
     .then(dbPackageData => res.json(dbPackageData))
