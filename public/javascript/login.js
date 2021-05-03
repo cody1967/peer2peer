113346bc033f759f0385a8c670ba1f7fa13321f1
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-05-02 22:51:28 -0600

    login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -5,7 +5,7 @@ async function loginFormHandler(event) {
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    const response = await fetch('/api/client/login', {
+    const response = await fetch('/api/client', {
       method: 'post',
       body: JSON.stringify({
         email,
