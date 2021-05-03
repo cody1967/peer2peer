6271de76a6e2a0b319d3987fd8dbeec0ef3981bd
unknown <maxwellburden@gmail.com>
2021-05-03 01:27:19 -0600

    cookies to website

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 const routes = require('./controllers');
 const sequelize = require('./config/connection.js');
 
@@ -11,6 +12,44 @@ app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening' + PORT));
   });
+
+  const cookieStorage = {
+    getItem: (key) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('+'))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[key]
+    },
+    setItem: (key, value) => {
+        document.cookie = '${key}=${value}'
+    },
+}
+
+const storageType = cookieStorage;
+const consentPropertyName = 'jdc_consent';
+
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+    const consentPopup = document.getElementById('consent-popup');
+    const acceptBtn = document.getElementById('accept');
+
+    const acceptFn = event => {
+        saveToStorage(storageType)
+        consentPopup.classList.add('hidden');
+    };
+
+    acceptBtn.addEventListener('click', acceptFn);
+
+
+    if (shouldShowPopup(storageType)) {
+        setTimeout(() => {
+            consentPopup.classList.remove('hidden');
+       }, 2000);
+    }
+};
 
