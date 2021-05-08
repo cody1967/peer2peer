3894c752bd45dda801562f50fe0a4090ea7565b4
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-05-08 00:35:03 -0600

    calender

---
+++ public/javascript/driver.js
@@ -0,0 +1,55 @@
+async function driverjobs(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/clients/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/packages');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const first_name = document.querySelector('#firstname-signup').value.trim();
+  const last_name = document.querySelector('#lastname-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (first_name && last_name && email && password) {
+    const response = await fetch('/api/clients', {
+      method: 'post',
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/drivers');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', driverjobs);
+
+document.querySelector('.signup-form').addEventListener('submit', driverjobs);
